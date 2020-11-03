dd8ae3820c46afc736783b34e42e406604a4a5ee
ruhollah_jafari <jruhollah@gmail.com>
2020-11-04 00:39:05 +0330

    Add project files.

---
+++ Client_App/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer.Context;
+using DataLayer.Models;
+using DataLayer.Repositories;
+using DataLayer.Services;
+
+namespace Client_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MyContext db=new MyContext();
+
+            //Person p=new Person()
+            //{
+            //   Name = "googooli
+            //    Family = "magooli
+            //    WebSite = "test.com"
+            //};
+            //db.Persons.Add(p);
+            //db.SaveChanges();
+
+            MyContext db = new MyContext();
+            IPersonRepository personRepository=new PersonRepository(db);
+            personRepository.InsertPerson(new Person()
+            {
+                Name = "Ruhollah",
+                Family = "Jafari",
+                WebSite = "jafari94.ir"
+            });
+            personRepository.Save();
+            db.Dispose();
+        }
+    }
+}
